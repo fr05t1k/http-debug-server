f5215df02ce3a168b8f286465db0caad599e4280
Stanislav Pavlovichev <fr05t1k4@gmail.com>
2021-10-12 23:26:10 -0400

    update formula

--- Formula/http-debug-server.rb
+++ Formula/http-debug-server.rb
@@ -3,8 +3,8 @@ require "language/go"
 class HttpDebugServer < Formula
   desc ""
   homepage "https://github.com/fr05t1k/http-debug-server"
-  url "https://github.com/fr05t1k/http-debug-server/archive/refs/tags/v1.0.tar.gz"
-  sha256 "8dc5127b68a016f9276f992ed549b46ce3bc4431caf34dbc7439b28777b4b71f"
+  url "https://github.com/fr05t1k/http-debug-server/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "2df6ea7db08ebbff2ce23da8df918febecf4450872380049074681c6d26f4714"
   head "https://github.com/fr05t1k/http-debug-server.git"
   depends_on "go" => :build
 
@@ -13,7 +13,7 @@ class HttpDebugServer < Formula
     path = buildpath/"src/github.com/fr05t1k/http-debug-server"
     path.install Dir["*"]
     cd path do
-      system "go", "build", "-o", "#{bin}/http-debug-server"
+      system "go", "build", "-ldflags", "-X 'main.Version=v#{version}'", "-o", "#{bin}/http-debug-server"
     end
   end
 
